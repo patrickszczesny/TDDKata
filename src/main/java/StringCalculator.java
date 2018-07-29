c2782dabd1cb58d06cc119bafb45668914318e5c
Sakwer <patryk-szczesny@o2.pl>
2018-07-29 22:13:49 +0200

    2 Kata - In around 43 min finish at 3.2 passed

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -4,25 +4,21 @@ import java.util.List;
 class StringCalculator {
 
     static int Add(String numbersInString) {
-        if( numbersInString.isEmpty()){
-           return 0;
+        if (numbersInString.isEmpty()) {
+            return 0;
         }
-
-        String[] stringTable=numbersInString.split(",");
+        String[] stringTable = numbersInString.split(",");
         List<String[]> stringFinalList = new ArrayList<>();
-
-        if (stringTable.length>1) {
+        if (stringTable.length > 1) {
             for (int i = 0; i < stringTable.length; i++) {
                 stringFinalList.add(stringTable[i].split("\n"));
             }
-
             int result = 0;
             for (int i = 0; i < stringFinalList.size(); i++) {
                 for (int j = 0; j < stringFinalList.get(i).length; j++) {
                     result += Integer.parseInt(stringFinalList.get(i)[j]);
                 }
             }
-
             return result;
         }
         return Integer.parseInt(numbersInString);
