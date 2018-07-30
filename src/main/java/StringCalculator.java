58b304a2618a7a0925cd859a0f148bf270a829d4
Sakwer <patryk-szczesny@o2.pl>
2018-07-30 22:20:31 +0200

    3 Kata - 1,2 pass

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,6 +1,9 @@
 class StringCalculator {
 
     public static int Add(String numbersInString) {
+        if (numbersInString.isEmpty()){
+            return 0;
+        }
         return Integer.parseInt(numbersInString);
     }
 }
