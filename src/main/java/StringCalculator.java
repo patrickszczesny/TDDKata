6e02e2323ff1b89e72f3c422f5a09af83add56f9
Sakwer <patryk-szczesny@o2.pl>
2018-07-31 23:06:12 +0200

    4 Kata -1,2,3,4 test pass

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,8 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class StringCalculator {
      static int Add(String numberInString) {
          if (numberInString.isEmpty()){
              return 0;
          }
+
+        String[] stringNumbersInForOfListOfStrings= numberInString.split(",");
+
+        if( stringNumbersInForOfListOfStrings.length>1){
+            int sum=0;
+            for (String stringNumbersInForOfListOfString : stringNumbersInForOfListOfStrings) {
+                sum += Integer.parseInt(stringNumbersInForOfListOfString);
+            }
+            return sum;
+        }
         return Integer.parseInt(numberInString);
     }
 }
