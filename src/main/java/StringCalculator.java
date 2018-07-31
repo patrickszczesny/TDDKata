d517fbc899e6407ec85be948f3a8b01f4c03b873
Sakwer <patryk-szczesny@o2.pl>
2018-07-31 23:15:00 +0200

    4 Kata -1,2,3,4,5,6 test pass - reformat

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,27 +2,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 class StringCalculator {
-     static int Add(String numberInString) {
-         if (numberInString.isEmpty()){
-             return 0;
-         }
-
-        String[] stringNumbersInForOfListOfStrings= numberInString.split(",");
-
+    static int Add(String numberInString) {
+        if (numberInString.isEmpty()) {
+            return 0;
+        }
+        String[] stringNumbersInForOfListOfStrings = numberInString.split(",");
         List<String[]> listOfStringTables = new ArrayList<>();
-         for (int i = 0; i <stringNumbersInForOfListOfStrings.length ; i++) {
-             listOfStringTables.add(stringNumbersInForOfListOfStrings[i].split("\n"));
-         }
+        for (String stringNumbersInForOfListOfString : stringNumbersInForOfListOfStrings) {
+            listOfStringTables.add(stringNumbersInForOfListOfString.split("\n"));
+        }
+        if (stringNumbersInForOfListOfStrings.length > 1 || listOfStringTables.get(0).length > 1) {
 
-
-        if( stringNumbersInForOfListOfStrings.length>1||listOfStringTables.get(0).length>1){
-
-            int sum=0;
-             for (int i = 0; i <listOfStringTables.size() ; i++) {
-                 for (int j = 0; j <listOfStringTables.get(i).length ; j++) {
-                     sum += Integer.parseInt(listOfStringTables.get(i)[j]);
-                 }
-             }
+            int sum = 0;
+            for (String[] listOfStringTable : listOfStringTables) {
+                for (String aListOfStringTable : listOfStringTable) {
+                    sum += Integer.parseInt(aListOfStringTable);
+                }
+            }
             return sum;
         }
         return Integer.parseInt(numberInString);
