d95c4c05fc7ce731bda25d8065b2758116285a95
Sakwer <patryk-szczesny@o2.pl>
2018-07-31 23:14:17 +0200

    4 Kata -1,2,3,4,5,6 test pass

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -9,11 +9,20 @@ class StringCalculator {
 
         String[] stringNumbersInForOfListOfStrings= numberInString.split(",");
 
-        if( stringNumbersInForOfListOfStrings.length>1){
+        List<String[]> listOfStringTables = new ArrayList<>();
+         for (int i = 0; i <stringNumbersInForOfListOfStrings.length ; i++) {
+             listOfStringTables.add(stringNumbersInForOfListOfStrings[i].split("\n"));
+         }
+
+
+        if( stringNumbersInForOfListOfStrings.length>1||listOfStringTables.get(0).length>1){
+
             int sum=0;
-            for (String stringNumbersInForOfListOfString : stringNumbersInForOfListOfStrings) {
-                sum += Integer.parseInt(stringNumbersInForOfListOfString);
-            }
+             for (int i = 0; i <listOfStringTables.size() ; i++) {
+                 for (int j = 0; j <listOfStringTables.get(i).length ; j++) {
+                     sum += Integer.parseInt(listOfStringTables.get(i)[j]);
+                 }
+             }
             return sum;
         }
         return Integer.parseInt(numberInString);
