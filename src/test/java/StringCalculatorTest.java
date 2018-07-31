8c1dabb106c8fd1d61efa4781570a03a2e8613b5
Sakwer <patryk-szczesny@o2.pl>
2018-07-31 22:51:56 +0200

    4 Kata -Start

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -4,122 +4,15 @@ import org.junit.jupiter.api.Test;
 
 class StringCalculatorTest {
 
-    @DisplayName("Should return 0 if given null String")
+    @DisplayName("")
     @Test
     void test01() {
         //given
-        String numbersInString = "";
-        //when
-        int resultOfSum = StringCalculator.Add(numbersInString);
-        //then
-        Assertions.assertThat(resultOfSum).isEqualTo(0);
-    }
 
-    @DisplayName("Should return 1 if given 1 in String")
-    @Test
-    void test02() {
-        //given
-        String numbersInString = "1";
         //when
-        int resultOfSum = StringCalculator.Add(numbersInString);
-        //then
-        Assertions.assertThat(resultOfSum).isEqualTo(1);
-    }
 
-    @DisplayName("Should return 33 if given 33 in String")
-    @Test
-    void test03() {
-        //given
-        String numbersInString = "33";
-        //when
-        int resultOfSum = StringCalculator.Add(numbersInString);
         //then
-        Assertions.assertThat(resultOfSum).isEqualTo(33);
-    }
 
-    @DisplayName("Should return 3 if given two number 1,2 in String")
-    @Test
-    void test04() {
-        //given
-        String numbersInString = "1,2";
-        //when
-        int resultOfSum = StringCalculator.Add(numbersInString);
-        //then
-        Assertions.assertThat(resultOfSum).isEqualTo(3);
-    }
-
-    @DisplayName("Should return 10 if given two number 7,3 in String")
-    @Test
-    void test05() {
-        //given
-        String numbersInString = "7,3";
-        //when
-        int resultOfSum = StringCalculator.Add(numbersInString);
-        //then
-        Assertions.assertThat(resultOfSum).isEqualTo(10);
-    }
-
-    @DisplayName("Should return 6 if given three number 1,2,3 in String")
-    @Test
-    void test06() {
-        //given
-        String numbersInString = "1,2,3";
-        //when
-        int resultOfSum = StringCalculator.Add(numbersInString);
-        //then
-        Assertions.assertThat(resultOfSum).isEqualTo(6);
-    }
-
-    @DisplayName("Should return 10 if given 10 number 1,1,1,1,1,1,1,1,1,1 in String")
-    @Test
-    void test07() {
-        //given
-        String numbersInString = "1,1,1,1,1,1,1,1,1,1";
-        //when
-        int resultOfSum = StringCalculator.Add(numbersInString);
-        //then
-        Assertions.assertThat(resultOfSum).isEqualTo(10);
-    }
-
-    @DisplayName("Should return 3 if given 2 number 1\n2 in String")
-    @Test
-    void test08() {
-        //given
-        String numbersInString = "1\n2";
-        //when
-        int resultOfSum = StringCalculator.Add(numbersInString);
-        //then
-        Assertions.assertThat(resultOfSum).isEqualTo(3);
-    }
-    @DisplayName("Should return 6 if given 2 number 1\n2,3 in String")
-    @Test
-    void test09() {
-        //given
-        String numbersInString = "1\n2,3";
-        //when
-        int resultOfSum = StringCalculator.Add(numbersInString);
-        //then
-        Assertions.assertThat(resultOfSum).isEqualTo(6);
-    }
-    @DisplayName("Should return 10 if given 2 number 1,2\n3,4 in String")
-    @Test
-    void test010() {
-        //given
-        String numbersInString = "1,2\n3,4";
-        //when
-        int resultOfSum = StringCalculator.Add(numbersInString);
-        //then
-        Assertions.assertThat(resultOfSum).isEqualTo(10);
-    }
-    @DisplayName("Should return 10 if given 2 number 1\n2,3\n4 in String")
-    @Test
-    void test011() {
-        //given
-        String numbersInString = "1\n2,3\n4";
-        //when
-        int resultOfSum = StringCalculator.Add(numbersInString);
-        //then
-        Assertions.assertThat(resultOfSum).isEqualTo(10);
     }
 
 }
